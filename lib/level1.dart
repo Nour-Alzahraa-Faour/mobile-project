eb885363a1a19588ee8eae0cd48d3269965f453d
Nour-Alzahraa-Faour <nourfaour700@gmail.com>
2024-07-27 14:56:30 +0300

    Update level1.dart

--- lib/level1.dart
+++ lib/level1.dart
@@ -11,19 +11,20 @@ class Level1 extends StatefulWidget {
 int score = 0;
 List<Animal> list1 = [];
 List<Animal> list2 = [];
+
 void initLevel() {
   score = 0;
   list1 = [
-    Animal(name: 'Camel', img: '.idea/assets/camel.png', voice: 'none'),
-    Animal(name: 'Cat', img: '.idea/assets/cat.png', voice: 'none'),
-    Animal(name: 'Cow', img: '.idea/assets/cow.png', voice: 'none'),
-    Animal(name: 'Dog', img: '.idea/assets/dog.png', voice: 'none'),
-    Animal(name: 'Fox', img: '.idea/assets/fox.png', voice: 'none'),
-    Animal(name: 'Hen', img: '.idea/assets/hen.png', voice: 'none'),
-    Animal(name: 'Horse', img: '.idea/assets/horse.png', voice: 'none'),
-    Animal(name: 'Lion', img: '.idea/assets/lion.png', voice: 'none'),
-    Animal(name: 'Panda', img: '.idea/assets/panda.png', voice: 'none'),
-    Animal(name: 'Sheep', img:'.idea/assets/sheep.png', voice: 'none'),
+    Animal(name: 'Camel', img: 'lib/assets/camel.png', voice: 'none'),
+    Animal(name: 'Cat', img: 'lib/assets/cat.png', voice: 'none'),
+    Animal(name: 'Cow', img: 'lib/assets/cow.png', voice: 'none'),
+    Animal(name: 'Dog', img: 'lib/assets/dog.png', voice: 'none'),
+    Animal(name: 'Fox', img: 'lib/assets/fox.png', voice: 'none'),
+    Animal(name: 'Hen', img: 'lib/assets/hen.png', voice: 'none'),
+    Animal(name: 'Horse', img: 'lib/assets/horse.png', voice: 'none'),
+    Animal(name: 'Lion', img: 'lib/assets/lion.png', voice: 'none'),
+    Animal(name: 'Panda', img: 'lib/assets/panda.png', voice: 'none'),
+    Animal(name: 'Sheep', img: 'lib/assets/sheep.png', voice: 'none'),
   ];
   list2 = List<Animal>.from(list1);
 
@@ -38,122 +39,126 @@ class _Level1State extends State<Level1> {
     super.initState();
   }
 
+  void checkScore() {
+    if (score >= 100) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Congratulations!'),
+            content: Text('You have completed the level with a score of 100!'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-
-
-
-        child: Column(
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Row(
             children: [
-              SizedBox(
-                height: 20,
+              SizedBox(width: 10),
+              Text(
+                'Score: ',
+                style: TextStyle(fontSize: 20),
               ),
-              Center(
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'Score: ',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                    Text(
-                      score.toString(),
-                      style: TextStyle(fontSize: 30, color: Colors.green),
-                    ),
-                    SizedBox(
-                      width: 240,
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        initLevel();
-                        setState(() {});
-                      },
-                      icon: Icon(
-                        Icons.restart_alt,
-                        color: Colors.white,
-                      ),
-                      style: IconButton.styleFrom(
-                        backgroundColor: Colors.orange,
-                      ),
-                    ),
-                  ],
+              Text(
+                score.toString(),
+                style: TextStyle(fontSize: 30, color: Colors.green),
+              ),
+              SizedBox(width: 240),
+              IconButton(
+                onPressed: () {
+                  initLevel();
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.restart_alt,
+                  color: Colors.white,
                 ),
+                style: IconButton.styleFrom(backgroundColor: Colors.orange),
               ),
-              Divider(
-                indent: 0,
-                endIndent: 260,
-                color: Colors.orange,
+            ],
+          ),
+          Divider(indent: 0, endIndent: 260, color: Colors.orange),
+          Row(
+            children: [
+              Column(
+                children: list1.map((animal1) {
+                  return Container(
+                    child: Draggable<Animal>(
+                      data: animal1,
+                      childWhenDragging: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        backgroundImage: AssetImage(animal1.img),
+                        radius: 50,
+                      ),
+                      feedback: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        backgroundImage: AssetImage(animal1.img),
+                        radius: 30,
+                      ),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        backgroundImage: AssetImage(animal1.img),
+                        radius: 30,
+                      ),
+                    ),
+                  );
+                }).toList(),
               ),
-              Row(
-                children: [
-                  Column(
-                    children: list1.map((animal1) {
-                      return Container(
-
-                          child:
-                          Draggable<Animal>(
-                              data: animal1,
-                              childWhenDragging: CircleAvatar(
-                                backgroundColor: Colors.white,
-                                backgroundImage: AssetImage(animal1.img),
-                                radius: 50,
-                              ),
-                              feedback:CircleAvatar(
-                                backgroundColor: Colors.white,
-                                backgroundImage: AssetImage(animal1.img),
-                                radius: 30,
-                              ),
-                              child: CircleAvatar(
-                                backgroundColor: Colors.white,
-                                backgroundImage: AssetImage(animal1.img),
-                                radius: 30,
-                              )
-                          ));
-                    }).toList(),
-                  ),
-                  SizedBox(
-                    width: 200,
-                  ),
-
-                  Column(
-                    children: list2.map((animal2) {
-                      return DragTarget<Animal>(
-                          onAcceptWithDetails: (animal1) {
-                            if (animal1.data.name == animal2.name) {
-                              setState(() {
-                                list1.remove(animal2);
-                                list2.remove(animal2);
-
-                              });
-                              score = score + 10;
-
-                            }
-                          }, builder: (context, _,__) {
-                      return Container(
-                      width: 100,
-                      height: 100,
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      child:Text(animal2.name,style: Theme.of(context).textTheme.headlineSmall,),
+              SizedBox(width: 200),
+              Column(
+                children: list2.map((animal2) {
+                  return DragTarget<Animal>(
+                    onAcceptWithDetails: (details) {
+                      Animal animal1 = details.data;
+                      if (animal1.name == animal2.name) {
+                        setState(() {
+                          list1.remove(animal1);
+                          list2.remove(animal2);
+                          score += 10;
+                          checkScore();  // Check score after updating it
+                        });
+                      }
+                      else {
+                        setState(() {
+                          score -= 5;
+                          if (score < 0) score = 0; // Ensure score doesn't go negative
+                        });
+                      }
+                    },
+                    builder: (context, candidateData, rejectedData) {
+                      return Row(
+                        children: [
+                          SizedBox(width: 10),
+                          Text(
+                            animal2.name,
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                          SizedBox(height: 70),
+                        ],
                       );
-                      });
-                    }).toList(),
-
-                  ),
-                ],
-              )
-
-            ]
-        )
+                    },
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
-
-
-
-
-
   }
 }
